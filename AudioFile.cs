e17ddf97cf0ae1a71605a2ca701b5c624a129cd0
Rick Cable <rick.cable@gmail.com>
2024-02-12 15:25:53 -0800

    Create AudioFile.cs

---
+++ AudioFile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDRipperExample
+{
+    class AudioFile
+    {
+        public string Title { get; set; }
+        public string Volume { get; set; }
+        public string DriveLetter { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
