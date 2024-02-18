63deda5d4330883ddee53c2b50aa562b9a993463
Rick Cable <rick.cable@gmail.com>
2024-02-17 17:21:50 -0800

    Update AudioFile.cs
    Updated the volume to be stored natively in a float to save on conversion code in the refactoring of the GetVolume() method

--- AudioFile.cs
+++ AudioFile.cs
@@ -7,13 +7,17 @@ namespace MP3Jukebox
     class AudioFile
     {
         public string Title { get; set; }
-        public string Volume { get; set; }
+        public float Volume { get; set; }
         public string DriveLetter { get; set; }
         public string FilePath { get; set; }
         public string CurrentUser { get; set; }
         public string UserHomeFolder { get; set; }
         public string[] AudioFileCollection { get; set; }
+        public string[] MetaDataCollection { get; set; }
         public int RandomNumber { get; set; }
         public string SearchTerm { get; set; }
+        public int CustomCollectionCounter { get; set; }
+        public bool IsInAutoPlayMode { get; set; }
+        public int AutoPlayCounter { get; set; }
     }
 }
